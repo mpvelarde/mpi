ee862231f1e77ce31b3724b16405fff7d81ec920
María Velarde <mariavelarde@Marias-MacBook-Pro.local>
2016-03-02 15:42:18 +0000

    worker farmer interaction test 1

--- aquadSolution.c
+++ aquadSolution.c
@@ -156,7 +156,7 @@ void worker(int mypid) {
         tasksdone++;
         
         // Send result
-        //MPI_Send(result, 2, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
+        MPI_Send(result, 2, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
     
         // Receive next task
         //MPI_Recv(&task, 5, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
