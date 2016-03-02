d6b3abf7d9eab38e586c283b648698d755930905
María Velarde <mariavelarde@Marias-MacBook-Pro.local>
2016-03-02 18:10:24 +0000

    worker farmer interaction test 1

--- aquadSolution.c
+++ aquadSolution.c
@@ -11,8 +11,7 @@
 
 #define SLEEPTIME 1
 
-#define MAX_TASKS 100
-#define NO_MORE_TASKS MAX_TASKS+1
+#define NO_MORE_TASKS -1
 
 int *tasks_per_process;
 
@@ -71,47 +70,69 @@ double farmer(int numprocs) {
     stack *tasks = new_stack();
     
     // Init variables to receive info from MPI_Recv
-    int tag, who;
+    int i, tag, who;
     MPI_Status status;
-    double area[MAX_TASKS], result;
-    double points[5], temp[2];
+    double result;
+    double points[5], temp[5];
     double *task;
+    double left, mid, fleft, fmid, larea;
+    double mid, right, fmid, fright, rarea;
+    double taskCounter = 0;
     
-    // Generate tasks
-    int i = 0;
-    while(i < MAX_TASKS) {
-        points[0] = A;
-        points[1] = B;
-        points[2] = F(A);
-        points[3] = F(B);
-        points[4] = (F(A)+F(B)) * (B-A)/2;
-        
-        push(points, tasks);
-        i++;
-    }
+    // Generate first task
+    points[0] = A;
+    points[1] = B;
+    points[2] = F(A);
+    points[3] = F(B);
+    points[4] = (F(A)+F(B)) * (B-A)/2;
+    push(points, tasks);
+    taskCounter++;
     
-    // Assume at least as many tasks as workers
-    for (i=0; i < (numprocs-1); i++) {
+    while (!is_empty(tasks)) {
+        i = (rand() % (numprocs-2)) + 1;
         task = pop(tasks);
-        MPI_Send(task, 5, MPI_DOUBLE, i+1, i, MPI_COMM_WORLD);
-    }
-    
-    while (i<MAX_TASKS) {
-        MPI_Recv(&temp, 2, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        MPI_Send(task, 5, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+        
+        MPI_Recv(&temp, 5, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
         who = status.MPI_SOURCE;
         tag = status.MPI_TAG;
-        result += temp[0] + temp[1];
+        larea = temp[0];
+        rarea = temp[1];
         
-        task = pop(tasks);
-        MPI_Send(task, 5, MPI_DOUBLE, who, i, MPI_COMM_WORLD);
-        i++;
+        // Create more tasks or save result
+        if (temp[2] != -1 && temp[3] != -1 && temp[4] != -1){
+            
+            fleft = F(temp[2]);
+            fmid = F(temp[3]);
+            fright = F(temp[4]);
+            
+            points[0] = left;
+            points[1] = mid;
+            points[2] = fleft;
+            points[3] = fmid;
+            points[4] = larea;
+            push(points, tasks);
+            taskCounter++;
+            
+            points[0] = mid;
+            points[1] = right;
+            points[2] = fmid;
+            points[3] = fright;
+            points[4] = rarea;
+            push(points, tasks);
+            taskCounter++;
+        }else{
+            result += larea + rarea;
+        }
     }
     
     for (i=0; i < (numprocs-1); i++) {
-        MPI_Recv(&temp, 2, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        MPI_Recv(&temp, 4, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
         who = status.MPI_SOURCE;
         tag = status.MPI_TAG;
-        result += temp[0] + temp[1];
+        larea = temp[0];
+        rarea = temp[1];
+        result += larea + rarea;
      
         MPI_Send(task, 5, MPI_DOUBLE, who, NO_MORE_TASKS, MPI_COMM_WORLD);
     }
@@ -127,7 +148,7 @@ void worker(int mypid) {
     // Init variables to receive task
     double task[5];
     int tag;
-    double result[2];
+    double result[5];
     double left, right, fleft, fright, lrarea;
     double mid, fmid, larea, rarea;
     MPI_Status status;
@@ -148,15 +169,29 @@ void worker(int mypid) {
         fmid = F(mid);
         
         // Get result
-        result[0] = (fleft + fmid) * (mid - left) / 2;
-        result[1] = (fmid + fright) * (right - mid) / 2;
+        larea = (fleft + fmid) * (mid - left) / 2;
+        rarea = (fmid + fright) * (right - mid) / 2;
+        
+        result[0] = larea;
+        result[1] = rarea;
+        
+        if( fabs((larea + rarea) - lrarea) > EPSILON ) {
+            result[2] = left;
+            result[3] = mid;
+            result[4] = right;
+        }else{
+            result[2] = -1;
+            result[3] = -1;
+            result[4] = -1;
+        }
         
         // Update counters
-        workdone+= result[0] + result[1];
+        workdone+= result[0];
         tasksdone++;
+        tasks_per_process[mypid] = tasksdone;
         
         // Send result
-        MPI_Send(result, 2, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
+        MPI_Send(result, 5, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
     
         // Receive next task
         MPI_Recv(&task, 5, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
@@ -164,19 +199,4 @@ void worker(int mypid) {
     }
     printf("Worker %d solved %d tasks totalling %f units of work \n", mypid, tasksdone, workdone);
 }
-
-double quad(double left, double right, double fleft, double fright, double lrarea) {
-    double mid, fmid, larea, rarea;
-    
-    mid = (left + right) / 2;
-    fmid = F(mid);
-    larea = (fleft + fmid) * (mid - left) / 2;
-    rarea = (fmid + fright) * (right - mid) / 2;
-    if( fabs((larea + rarea) - lrarea) > EPSILON ) {
-        larea = quad(left, mid, fleft, fmid, larea);
-        rarea = quad(mid, right, fmid, fright, rarea);
-    }
-    return (larea + rarea);
-}
-
 
