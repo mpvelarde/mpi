d0ea39d03b070dd3c073580b091facc5c6953b29
María Velarde <mariavelarde@Marias-MacBook-Pro.local>
2016-03-02 15:37:53 +0000

    worker farmer interaction test 1

--- aquadSolution.c
+++ aquadSolution.c
@@ -136,9 +136,9 @@ void worker(int mypid) {
     MPI_Recv(&task, 5, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
     tag = status.MPI_TAG;
     
-    while (tag != NO_MORE_TASKS) {
+    //while (tag != NO_MORE_TASKS) {
         // Get variables
-        /*left = task[0];
+        left = task[0];
         right = task[1];
         fleft = task[2];
         fright = task[3];
@@ -156,12 +156,12 @@ void worker(int mypid) {
         tasksdone++;
         
         // Send result
-        MPI_Send(result, 2, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
-        */
+        //MPI_Send(result, 2, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
+        
         // Receive next task
-        MPI_Recv(&task, 5, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        tag = status.MPI_TAG;
-    }
+        //MPI_Recv(&task, 5, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        //tag = status.MPI_TAG;
+    //}
     printf("Worker %d solved %d tasks totalling %f units of work \n", mypid, tasksdone, workdone);
 }
 
