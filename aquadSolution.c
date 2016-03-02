53053d2026fadf13abba5a4dbaa0d33184035d24
María Velarde <mariavelarde@Marias-MacBook-Pro.local>
2016-03-02 15:51:40 +0000

    worker farmer interaction test 1

--- aquadSolution.c
+++ aquadSolution.c
@@ -107,7 +107,7 @@ double farmer(int numprocs) {
         i++;
     }
     
-    for (i=0; i < numprocs; i++) {
+    for (i=0; i < (numprocs-1); i++) {
         MPI_Recv(&temp, 2, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
         who = status.MPI_SOURCE;
         tag = status.MPI_TAG;
