47d727247451ebe53acb7a7f3dbe2943da8b0c3a
María Velarde <mariavelarde@Marias-MacBook-Pro.local>
2016-03-03 09:44:00 +0000

    recursive task creation

--- aquadSolution.c
+++ aquadSolution.c
@@ -77,7 +77,53 @@ double farmer(int numprocs) {
     double temp[5];
     double *task;
     
-    generateTask(tasks, A, B, F(A), F(B), (F(A)+F(B)) * (B-A)/2, numprocs);
+    // Generate first task
+    points[0] = A;
+    points[1] = B;
+    points[2] = F(A);
+    points[3] = F(B);
+    points[4] = (F(A)+F(B)) * (B-A)/2;
+    push(points, tasks);
+    
+    while (!is_empty(tasks)) {
+        i = (rand() % (numprocs-1)) + 1;
+        task = pop(tasks);
+        MPI_Send(task, 5, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+        
+        MPI_Recv(&temp, 5, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        who = status.MPI_SOURCE;
+        tag = status.MPI_TAG;
+        larea = temp[0];
+        rarea = temp[1];
+        tasks_per_process[tag] += 1;
+        
+        printf("Farmer received %f and %f from %d \n", larea, rarea, tag);
+        
+        // Create more tasks or save result
+        if (temp[2] != -1 && temp[3] != -1 && temp[4] != -1){
+            
+            fleft = F(temp[2]);
+            fmid = F(temp[3]);
+            fright = F(temp[4]);
+            
+            points[0] = left;
+            points[1] = mid;
+            points[2] = fleft;
+            points[3] = fmid;
+            points[4] = larea;
+            push(points, tasks);
+            
+            points[0] = mid;
+            points[1] = right;
+            points[2] = fmid;
+            points[3] = fright;
+            points[4] = rarea;
+            push(points, tasks);
+        }else{
+            result += larea + rarea;
+        }
+        
+    }
     
     printf("No more tasks \n");
     for (i=0; i < (numprocs-1); i++) {
@@ -85,48 +131,6 @@ double farmer(int numprocs) {
     }
     
     return result;
-}
-
-double generateTask(stack *tasks, double a, double b, double fa, double fb, double abarea, int numprocs){
-    double points[5], temp[5];
-    double left, mid, fleft, larea;
-    double right, fmid, fright, rarea;
-    int i, tag, who;
-    MPI_Status status;
-    double *task;
-    
-    points[0] = a;
-    points[1] = b;
-    points[2] = fa;
-    points[3] = fb;
-    points[4] = abarea;
-    push(points, tasks);
-    
-    i = (rand() % (numprocs-1)) + 1;
-    task = pop(tasks);
-    printf("Farmer sends %f, %f, %f, %f, %f\n", task[0], task[1], task[2], task[3], task[4]);
-    MPI_Send(task, 5, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
-    
-    MPI_Recv(&temp, 5, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-    who = status.MPI_SOURCE;
-    tag = status.MPI_TAG;
-    larea = temp[0];
-    rarea = temp[1];
-    tasks_per_process[tag] += 1;
-    
-    // Create more tasks or save result
-    if (temp[2] != -1 && temp[3] != -1 && temp[4] != -1){
-        
-        fleft = F(temp[2]);
-        fmid = F(temp[3]);
-        fright = F(temp[4]);
-        printf("Farmer generates %f, %f, %f, %f, %f\n", left, mid, fleft, fmid, larea);
-        generateTask(tasks, left, mid, fleft, fmid, larea, numprocs);
-        printf("Farmer generates %f, %f, %f, %f, %f\n", mid, right, fmid, fright, rarea);
-        generateTask(tasks, mid, right, fmid, fright, rarea, numprocs);
-    }else{
-        return larea + rarea;
-    }
 }
 
 void worker(int mypid) {
